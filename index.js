59f93121c5fc0166ddf3a2aff32a5b7ceaf7095e
Justin Berger Howes <howes.j.j@gmail.com>
2021-05-04 20:54:53 +1000

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your code here!
+const element = document.createElement( 'div' );
+document.body.appendChild(element);
+
+const ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+element.appendChild(ul);
+
+element.style.backgroundColor = '#27647B';
+
+element.style.fontSize = '24px';
+element.style.marginLeft = '30px';
+element.style.lineHeight = 2;
+
+document.getElementById('main').remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
